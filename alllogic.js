79c41d582d9389be04e20a31cd31c8927a350983
sophia <thangneimawi@gmail.com>
2022-04-29 15:36:05 +0630

    [new card designs added]

---
+++ alllogic.js
@@ -0,0 +1,16 @@
+
+
+// card2.html card 2 logic
+$(document).ready(function(){
+    $('.counter-value').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 3500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
